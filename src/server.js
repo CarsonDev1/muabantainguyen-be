60aa0ff95290cf1d05d3049f5f6352d610db2a1c
CarsonDev1 <buitritinht@gmail.com>
2025-10-08 22:05:42 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -42,7 +42,15 @@ const corsOptions = {
 	],
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+	allowedHeaders: [
+		'Content-Type',
+		'Authorization',
+		'X-Requested-With',
+		'Accept',
+		'Origin',
+		'refreshtoken',
+		'refreshToken',
+	],
 	exposedHeaders: ['Content-Range', 'X-Content-Range'],
 	maxAge: 600,
 };
