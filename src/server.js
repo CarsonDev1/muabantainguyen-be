40712f2a7e611b0cd909805cf91eaa7cca42b968
CarsonDev1 <buitritinht@gmail.com>
2025-10-08 21:32:49 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -31,37 +31,6 @@ import walletRoutes from './routes/walletRoutes.js';
 
 const app = express();
 
-app.use(helmet());
-app.use(
-	cors({
-		origin: function (origin, callback) {
-			// Allow requests with no origin (mobile apps, curl, postman)
-			if (!origin) return callback(null, true);
-
-			const allowedOrigins = [
-				'http://localhost:3000',
-				'http://localhost:3001',
-				'https://shopmmo.pro.vn',
-				'http://shopmmo.pro.vn',
-				'https://tainguyenmmoshop.com',
-			];
-
-			if (allowedOrigins.indexOf(origin) !== -1) {
-				callback(null, true);
-			} else {
-				callback(null, true); // Allow all for now
-			}
-		},
-		credentials: true,
-		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-		allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-		exposedHeaders: ['Content-Range', 'X-Content-Range'],
-		maxAge: 600,
-	})
-);
-
-// Add OPTIONS handler for preflight
-app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
